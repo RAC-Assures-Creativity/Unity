c4f90034f60e7960ae0ad40615660e3b9acdf290
Raisa Alejnadra Silva Plata <raisa2201silva@gmail.com>
2020-11-30 12:00:11 -0400

    conexión del botón jugar con el escenario SelectionGame

---
+++ Assets/Scenes/Controlador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controlador : MonoBehaviour
+{
+    public void CambiarEscena(string nombre)
+    {
+       print ("Cambiando de escena " + nombre);
+       SceneManager.LoadScene(nombre);
+    }
+	public void salir()
+    {
+        print ("saliendo");
+        Application.Quit();
+    }
+}
